35dfcd659ed5eb225faa47248485ce452f18d18a
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-05 12:28:08 +0530

    Add files via upload

---
+++ TERNARY.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class TERNARY {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println((a<b)?"A is amaller":"B is smaller");
+    }
+    
+}
