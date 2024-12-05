6114d30a35d9648dc5ddc729cf441144d77c4106
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-05 15:54:50 +0530

    Add files via upload

---
+++ evenusingFOR.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class evenusingFOR
+{
+    public static void main(String args[])
+    {
+         for(int i=0;i<=10;i+=2)
+         {
+            System.out.println(i);
+        
+         }
+    }
+}
+
