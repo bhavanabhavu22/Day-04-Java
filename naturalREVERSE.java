092318ce5e93aff96c3efe21b5c471c6a1cf5e54
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-05 23:41:29 +0530

    Add files via upload

---
+++ naturalREVERSE.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class naturalREVERSE{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=5;
+        while(i>=0)
+        {
+            System.out.println(i);
+            i--;
+
+        }
+    }
+}
